acf0dadd497770fb8bd0ba7ed71df370dfd386a7
DESCHR0250 <DESCHR0250@7JSZHK2.ad.uauth.net>
2018-02-16 15:07:25 -0500

    Added prototype

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../com/rispacs/view/LoginView.fxml"));
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../com/rispacs/view/Prototype.fxml"));
+			Scene scene = new Scene(root,1400,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
