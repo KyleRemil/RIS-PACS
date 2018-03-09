9ab56a314737a4cac99a745dd818fdeeb95a0595
robccromer <robcaryc@gmail.com>
2018-03-08 20:26:54 -0500

    Basic Raw Patient Insert w/ DB

--- src/com/rispacs/model/DatabaseHandler.java
+++ src/com/rispacs/model/DatabaseHandler.java
@@ -1,4 +1,23 @@
 package com.rispacs.model;
 
-public class DatabaseHandler {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler
+{
+	private static String connectionString = "jdbc:mysql://localhost:3306/risdatabase?autoReconnect=true&useSSL=false";
+	private static String connectionName = "root";
+	private static String connectionPassword = "#altf4ctrlf6f12!";
+
+	// connection  method that connects us to the MySQL database
+	public static Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(connectionString, connectionName, connectionPassword);
+	}
+	// method that displays our errors in more detail if connection fails
+	public static void displayException(SQLException ex){
+		System.err.println("Error Message: " + ex.getMessage());
+		System.err.println("Error Code: " + ex.getErrorCode());
+		System.err.println("SQL Status: " + ex.getSQLState());
+	}
 }
