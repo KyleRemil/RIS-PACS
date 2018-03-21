be38a878b04b138ebabb4fb04e40317ffaa2f5d0
Tai-Iro <daniel.e.schrader.jr@gmail.com>
2018-03-20 22:35:37 -0400

    worked more on login
    I probably spent way too much time looking up MySQL syntax...

--- src/com/rispacs/controller/LoginController.java
+++ src/com/rispacs/controller/LoginController.java
@@ -1,16 +1,88 @@
 package com.rispacs.controller;
 
+//Takes username and password, and compares them to the DB
+//passwords are stored as 40-length strings after SHA1 hashing
+//info on hashing in Java: https://stackoverflow.com/questions/4895523/java-string-to-sha1
+
+
+import org.apache.commons.codec.digest.DigestUtils;
+
 import com.rispacs.model.Context;
 
+import application.DatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
-public class LoginController {
 
-	@FXML private TextField textField_username, textField_password;
+//imports for testing login view, without messing with main.java
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+//testing imports end
+
+
+public class LoginController  extends Application {
+	
+
+	//stuff for testing login view, without messing with main.java
+    public static void main(String[] args){
+        
+        try{
+	        //testing
+	        /*String pass = "Seven";
+	        String s1 = toSHA1( pass.getBytes("UTF-8") );
+	        String s2 = encryptPassword(pass);
+	        
+	        System.out.println("plaintext: "+pass + "\n" + "s1: " + s1+ "\ns2: "+s2);*/
+	        ArrayList<String> a = new ArrayList<String>();
+	        a.add("admin");
+	        a.add("Admin");
+	        a.add("RICHARD");
+	        a.add("JEFF");
+	        for(int i = 0; i < a.size(); i++)
+	        {
+	        	System.out.println( a.get(i) + " is:");
+	        	//encryptPassword(a.get(i));
+	        	//System.out.println("BigInt: " + encryptPassword2(a.get(i)));
+	        	System.out.println("apache 1: " + DigestUtils.sha1Hex(a.get(i).getBytes("UTF-8")));
+	        	System.out.println("apache 2: " + DigestUtils.sha1Hex(a.get(i)));
+	        	System.out.println();
+	        }
+        }
+        catch(Exception e){
+        	e.printStackTrace();
+        }
+      launch(args);
+    }
+
+    @Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../view/LoginView.fxml"));
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+    //end of testing stuff
+    
+
+	@FXML public TextField textField_username, textField_password;
 	@FXML private Label label_badUsername, label_badPassword;
     @FXML private Button button_login;
 
@@ -23,9 +95,125 @@ public class LoginController {
     @FXML
     void login(ActionEvent event) {
 
+    	System.out.println("Tried to login.");
+    	String s = "select (select if(count(*), 'match', 'no') from staff where username like ?) as user, "+
+    			"if(count(*), 'match', 'no') as pass, `staffID` as id, `staffName` as name, `StaffRole_staffRoleID` as r " +
+    			"from staff where username like ? and `password` like ?";
+    	// 1st two ?'s are the username (so have the same input), and 3rd is the hashed password (hash it first)
+    	String user = textField_username.getText();
+    	String pass = DigestUtils.sha1Hex(label_badPassword.getText());
+    	
+        Connection conn = null;
+    	PreparedStatement ps = null;
+        ResultSet rs = null;
+    	try{
+    		conn = DatabaseHandler.getConnection();
+			ps = conn.prepareStatement(s);
+			ps.setString(1, user);
+			ps.setString(2, user);
+			ps.setString(3, pass);
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next())
+			{
+				System.out.println(rs.getObject(1).getClass());
+			}
+		}
+    	catch (Exception e) {
+			e.printStackTrace();
+		}
+    	finally {
+        	try { rs.close(); } catch (Exception e) { /* ignored */ }
+            try { ps.close(); } catch (Exception e) { /* ignored */ }
+            try { conn.close(); } catch (Exception e) { /* ignored */ }
+       }
     }
 
     void setRole(int role) {
     	Context.getInstance().setRole(role);
     }
+    
+    //for testing hashing before I found the Apache Commons Codec
+//    //way2
+//    private static String encryptPassword(String password)
+//    {
+//        String sha1 = "";
+//        try
+//        {
+//            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+//            crypt.reset();
+//            crypt.update(password.getBytes("UTF-8"));
+//            byte[] b = crypt.digest();
+//            sha1 = byteToHex(b);//crypt.digest());
+//            System.out.println("way 1: "+ byteToHex(b));
+//            System.out.println("way 2: "+ byteArrayToHexString(b));
+//            System.out.println("way 3: "+ getHex(b));
+//            System.out.println("way 4: "+ getHexString(b));
+//        }
+//        catch(Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//        return sha1;
+//    }
+//
+//    //way 1
+//    private static String byteToHex(final byte[] hash)
+//    {
+//        Formatter formatter = new Formatter();
+//        for (byte b : hash)
+//        {
+//            formatter.format("%02x", b);
+//        }
+//        String result = formatter.toString();
+//        formatter.close();
+//        return result;
+//    }
+//    
+//    //way 2
+//    public static String byteArrayToHexString(byte[] b) {
+//    	  String result = "";
+//    	  for (int i=0; i < b.length; i++) {
+//    	    result +=
+//    	          Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+//    	  }
+//    	  return result;
+//	}
+//    
+//    //way 3
+//    static final String HEXES = "0123456789ABCDEF";
+//    public static String getHex( byte [] raw ) {
+//      if ( raw == null ) {
+//        return null;
+//      }
+//      final StringBuilder hex = new StringBuilder( 2 * raw.length );
+//      for ( final byte b : raw ) {
+//        hex.append(HEXES.charAt((b & 0xF0) >> 4))
+//           .append(HEXES.charAt((b & 0x0F)));
+//      }
+//      return hex.toString();
+//    }
+//    
+//    //way 4
+//    static final byte[] HEX_CHAR_TABLE = {
+//	    (byte)'0', (byte)'1', (byte)'2', (byte)'3',
+//	    (byte)'4', (byte)'5', (byte)'6', (byte)'7',
+//	    (byte)'8', (byte)'9', (byte)'a', (byte)'b',
+//	    (byte)'c', (byte)'d', (byte)'e', (byte)'f'
+//	};
+//
+//    public static String getHexString(byte[] raw) throws Exception
+//	{
+//    	byte[] hex = new byte[2 * raw.length];
+//    	int index = 0;
+//
+//    	for (byte b : raw) {
+//		  int v = b & 0xFF;
+//		  hex[index++] = HEX_CHAR_TABLE[v >>> 4];
+//	      hex[index++] = HEX_CHAR_TABLE[v & 0xF];
+//    	}
+//    	return new String(hex, "ASCII");
+//	}
+//    
 }
